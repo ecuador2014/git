3eaac002e5d65c9c3ac33bc184f5189d01c23816
ecuador2014 <palacios.cesar84@gmail.com>
2020-05-29 13:19:58 -0500

    Kotlin1

---
+++ test.kt
@@ -0,0 +1,6 @@
+/*mi primer programa en kotlin */
+
+fun main(args: Array<String>){
+    println("Hola Mundo bello")
+}
+
---
+++ variablesyconstantes.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var agus = 7
+
+    agus = 10
+
+    
+    val agustina = "agus"
+
+    println(agustina)
+
+}
+
